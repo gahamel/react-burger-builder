fa37c884e15bf943eacf3e77777c7c0d00c0a08b
gahamel <gaetanhamelpro@gmail.com>
2019-07-02 14:39:05 +0200

    change OrderSummary and Modal components to class to integrate lifeCycle control flow

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,27 +3,30 @@ import React from 'react';
 import Fragment from '../../../Fragment';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-        return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
-    })
+class OrderSummary extends React.Component {
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
 
-    return (
-        <Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with this ingredients :</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Total Price:{props.price}</p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success"clicked={props.purchaseContinued}>Continue</Button>
-        </Fragment>
-    );
-
- 
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+        return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+            })
+        return (
+            <Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with this ingredients :</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>Total Price:{this.props.price}</p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success"clicked={this.props.purchaseContinued}>Continue</Button>
+            </Fragment>
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
